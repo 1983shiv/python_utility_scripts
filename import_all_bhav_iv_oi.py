7b88af529c4f98a08cf027315b4ea318283a213a
= <=>
2024-07-04 06:06:29 +0530

    import all the required data at once

---
+++ import_all_bhav_iv_oi.py
@@ -0,0 +1,147 @@
+import csv
+from pymongo import MongoClient
+from datetime import datetime
+
+# Connect to MongoDB
+client = MongoClient('localhost', 27017)
+db = client['nse']
+stocks_collection = db['stocks']
+
+# Function to process OI data
+def process_and_update_fut_oi_data(file_path, expiry, expiryNext, type):
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            # date = datetime.strptime(row['Date'].strip(), '%Y-%m-%d')
+            expirydate = (row['XpryDt'])
+            intrument = (row['FinInstrmNm'])
+            if(intrument == type):
+            # FinInstrmNm
+                if(expirydate == expiry):
+                    date = datetime.strptime(row['RptgDt'].strip(), '%d-%b-%Y')
+                    symbol = row['TckrSymb']
+                    coi = int(float(row['ChngInOpnIntrst']))  # Parse coi as integer
+                    oi = int(float(row['OpnIntrst']))  
+
+                    # Construct expiry data
+                    expiry_data = {
+                        'expiry': expirydate,
+                        'coi': coi,
+                        'oi': oi
+                    }
+
+                    # Update document in nsestocks collection ('%d-%b-%Y') and '%Y-%m-%d'
+                    query = {'symbol': symbol, 'date': date}
+                    update = {
+                        '$push': {
+                            'drvt': {'$each': [expiry_data]}
+                        }
+                    }
+                    stocks_collection.update_one(query, update)
+                    # Update document in nsestocks collection ('%d-%b-%Y') and '%Y-%m-%d'
+                    query = {'symbol': symbol, 'date': date}
+                    update = {
+                        '$set': {
+                            'coi': coi,
+                            'oi': oi,
+                            'expiry': expiry
+                        }
+                    }
+                    stocks_collection.update_one(query, update)
+                    print(f'record update for - {date}')
+                else:
+                    print(f"some error occured {expirydate} and expiry {expiry}, {intrument} and {type} ")
+                    pass
+
+                if(expirydate == expiryNext):
+                    date = datetime.strptime(row['RptgDt'].strip(), '%d-%b-%Y')
+                    symbol = row['TckrSymb']
+                    coi = int(float(row['ChngInOpnIntrst']))  # Parse coi as integer
+                    oi = int(float(row['OpnIntrst']))  
+
+                    # Construct expiry data
+                    expiry_data = {
+                        'expiry': expirydate,
+                        'coi': coi,
+                        'oi': oi
+                    }
+
+                    # Update document in nsestocks collection ('%d-%b-%Y') and '%Y-%m-%d'
+                    query = {'symbol': symbol, 'date': date}
+                    update = {
+                        '$push': {
+                            'drvt': {'$each': [expiry_data]}
+                        }
+                    }
+                    stocks_collection.update_one(query, update)
+                    print(f'record update for - {date}')
+                else:
+                    print(f"some error occured {expirydate} and expiry {expiryNext}, {intrument} and {type} ")
+                    pass
+
+# Function to insert bhav data into mongodb
+def insert_or_update_document(collection, document):
+    collection.insert_one(document)
+    print("New document inserted for date:", document['date'])
+   
+# Function to update iv data
+def process_and_update_iv_data(file_path):
+    with open(file_path, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            # date = datetime.strptime(row['Date'].strip(), '%Y-%m-%d')
+            date = datetime.strptime(row['Date'].strip(), '%d-%b-%y') 
+            symbol = row['Symbol']
+            daily = None
+            if row['a'] != '-':
+                daily = round(float(row['a'])*100,2)
+            yearly = None
+            if row['b'] != '-':
+                yearly = round(float(row['b'])*100,2)
+
+
+            # Update document in nsestocks collection ('%d-%b-%Y') and '%Y-%m-%d'
+            query = {'symbol': symbol, 'date': date}
+            update = {
+                '$set': {
+                    'iv_daily': daily,
+                    'iv_yearly': yearly
+                }
+            }
+            stocks_collection.update_one(query, update)
+            print("IV data updated for date:", date, symbol)
+
+
+with open('sec_bhavdata_full_03072024.csv', 'r') as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+        if row[' SERIES'].strip() == 'EQ':  # Strip any extra spaces
+            document = {
+                'symbol': str(row['SYMBOL']).strip(),
+                'date': datetime.strptime(row[' DATE1'].strip(), '%d-%b-%Y'),
+                'prev_close': float(row[' PREV_CLOSE'].strip()),
+                'open_price': float(row[' OPEN_PRICE'].strip()),
+                'high_price': float(row[' HIGH_PRICE'].strip()),
+                'low_price': float(row[' LOW_PRICE'].strip()),
+                'last_price': float(row[' LAST_PRICE'].strip()),
+                'close_price': float(row[' CLOSE_PRICE'].strip()),
+                'ttl_trd_qnty': int(row[' TTL_TRD_QNTY'].strip()),
+                'turnover_lacs': float(row[' TURNOVER_LACS'].strip()),
+                'no_of_trades': int(row[' NO_OF_TRADES'].strip()),
+                'deliv_qty': int(row[' DELIV_QTY'].strip()),
+                'deliv_per': float(row[' DELIV_PER'].strip())
+            }
+            if stocks_collection is not None:
+                insert_or_update_document(stocks_collection, document)
+
+# Expiry Date
+expiryNext = '29-Aug-2024'
+expiry = '25-Jul-2024'
+type = 'FUTSTK'
+
+# Call and update IV data
+process_and_update_iv_data('CMVOLT_03072024.csv')
+# Call and update OI Data
+process_and_update_fut_oi_data('NSE_FO_bhavcopy_03072024.csv', expiry, expiryNext, type)
+
+# print("OI data processed and nse collection updated successfully")
